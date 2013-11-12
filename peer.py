c7c8a4aa41aef6746c33de8974b36c53b8d9b6ca
Chris Card <ccard@mymail.mines.edu>
2013-11-12 16:50:40 -0700

    now importing yaml for latencies

--- peer.py
+++ peer.py
@@ -8,29 +8,29 @@ import sys
 import json
 import os
 
+
 class Peer(DatagramProtocol):
-    
+
     UDP_IP = "239.6.6.6"
     UDP_PORT = 6666
     UDP_PORTSend = 6667
-        
+
     def startProtocol(self):
         # Set the TTL>1 so multicast will cross router hops:
         self.transport.setTTL(5)
         # Join a specific multicast group:
         self.transport.joinGroup(self.UDP_IP)
-                    
 
-    def datagramReceived(self,datagram, address):
+    def datagramReceived(self, datagram, address):
         print "Datagram %s received from %s" % (repr(datagram), repr(address))
         msg = json.loads(datagram)
         if msg["VERB"] == 'WANT' or msg["VERB"] == 'DOWNLOAD':
             files = os.listdir(os.getcwd())
             if msg["FILE"] in files:
-                res = self.compile_message("HAVE",msg["FILE"])
+                res = self.compile_message("HAVE", msg["FILE"])
                 self.transport.write(res, (self.UDP_IP, self.UDP_PORTSend))
 
-    def compile_message(self,verb,filename):
+    def compile_message(self, verb, filename):
         return json.dumps({"VERB": verb, "FILE": filename})
 
 if __name__ == "__main__":
@@ -38,8 +38,8 @@ if __name__ == "__main__":
     # We use listenMultiple=True so that we can run
     # multiple peers on the same machine
     reactor.listenMulticast(6666, Peer(),
-                        listenMultiple=True)
+                            listenMultiple=True)
     resource = File(os.getcwd())
     factory = Site(resource)
-    reactor.listenTCP(6666,factory)
+    reactor.listenTCP(6666, factory)
     reactor.run()
