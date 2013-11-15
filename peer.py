7bed1539ab49dc136e726633ced75281caa83a2b
Steven Ruppert <strupper@mines.edu>
2013-11-14 18:44:03 -0700

    Implement downloading in peer.py
    Works, though very finicky.

--- peer.py
+++ peer.py
@@ -1,26 +1,24 @@
 #! /usr/bin/env python
-
+import logging
 import os
 import errno
-import sys
-import json
 import time
 import threading
 import stat
+from os.path import realpath
+from sys import argv, exit
+from logging import debug, info, warn, error
 
-from twisted.internet.protocol import DatagramProtocol
+import requests
 from twisted.internet import reactor
 from twisted.web.static import File
 from twisted.web.server import Site
+from fuse import FUSE, FuseOSError, Operations, fuse_get_context
 
-from os.path import realpath
-from sys import argv, exit
-
-from fuse import FUSE, FuseOSError, Operations, LoggingMixIn, fuse_get_context
+logging.basicConfig(level=logging.DEBUG)
 
 
 class NapsterFilesystem(Operations):
-
     """
     Essentially a union filsystem between a local directory and the
     central server.
@@ -38,17 +36,30 @@ class NapsterFilesystem(Operations):
         self.local = realpath(local)
 
         self.created = time.time()
+        self.central_server = "bb136-10.mines.edu:6667"
 
-    # redirect all FUSE events to the same event with filenames scoped
-    # under the local directory, i.e. loopfs
-    # def __call__(self, op, path, *args):
-        # return super(NapsterFilesystem, self).__call__(
-                # op, self.local + path, *args)
+        self.central_files = self.get_central_files()
+        self.last_fetched = time.time()
 
-    def getattr(self, path, fh):
+    def get_central_files(self):
+        try:
+            files = requests.get('http://%s/' % self.central_server)
+        except requests.ConnectionError as e:
+            warn("Couldn't get files from central server: %s" % e)
+            return {}
+        if files.status_code is not 200:
+            warn("Couldn't get files from central server: %s" % files)
+            return {}
+        else:
+            files = files.json()
+            info("Received files from central server: %s", files)
+            return files
+
+    def getattr(self, path, fd):
+        debug("getattr on %s" % path)
+        uid, gid, _ = fuse_get_context()
         if path == '/':
-            uid, gid, _ = fuse_get_context()
-            return dict(st_mode=(stat.S_IFDIR | 0444),
+            return dict(st_mode=(stat.S_IFDIR | 0o444),
                         st_ctime=self.created,
                         st_mtime=self.created,
                         st_atime=self.created,
@@ -56,63 +67,94 @@ class NapsterFilesystem(Operations):
                         st_gid=gid,
                         st_nlink=2)
         else:
-            # TODO download remote if exists
-
-            # proxy to local
-            if os.path.exists(self.local + path):
+            debug("trying getattr %s locally..." % path)
+            try:
                 st = os.lstat(self.local + path)
                 return dict((key, getattr(st, key)) for key in (
                     'st_atime', 'st_ctime', 'st_gid', 'st_mode',
                     'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
+            except IOError:
+                info("File %s doesn't exist locally..." % path)
+                pass
+
+            # try getting remote
+
+            # TODO download remote if exists
+            filename = path[1:]
+            if filename in self.central_files:
+                debug("File %s exists on central server...", filename)
+                return dict(st_mode=(stat.S_IFREG | 0o444),
+                            st_ctime=self.created,
+                            st_mtime=self.created,
+                            st_atime=self.created,
+                            st_uid=65534, # nobody
+                            st_gid=65534, # nobody
+                            st_nlink=2)
             else:
                 raise FuseOSError(errno.ENOENT)
 
+
     # TODO cache files back in local directory if remote
     def open(self, path, flags):
-        return os.open(self.local + path, flags)
+        debug("trying opening %s locally..." % path)
+        try:
+            return os.open(self.local + path, flags)
+        except IOError:
+            info("File %s doesn't exist locally..." % path)
+            pass
+
+        # now try downloading it
+
+        filename = path[1:]
+        if filename in self.central_files:
+            debug("Downloading remote file %s" % path)
+            # TODO try different peer if not succeeds
+            peer = self.central_files[filename]["peers"][0]
+            try:
+                file = requests.get("http://%s/%s" % (peer, filename))
+            except requests.ConnectionError as e:
+                error("File %s could not be downloaded from %s: %s" % (filename, peer, e))
+                raise FuseOSError(errno.ENOENT)
+            if file.status_code is not 200:
+                warn("File %s could not be downloaded from %s " % (filename, peer))
+                raise FuseOSError(errno.ENOENT)
+            else:
+                info("downloaded %s from peer %s" % (filename, peer))
+                # copy file to local dir
+                debug("Writing file to %s" % (self.local + path))
+                local_file = open(self.local + path, 'w')
+                local_file.write(file.content)
+                local_file.close()
+                debug("saved %s from peer %s" % (filename, peer))
+
+                # now read it
+                return os.open(self.local + path, flags)
+        else:
+            raise FuseOSError(errno.ENOENT)
 
     def read(self, path, size, offset, fh):
         # file handle is from os.open in `open`, so use it directly
         os.lseek(fh, offset, 0)
         return os.read(fh, size)
 
-    # TODO union with central server file list
     def readdir(self, path, fh):
-        return ['.', '..'] + os.listdir(self.local + path)
+        debug("reading dir...")
+        if (time.time() - self.last_fetched) > 5:
+            info("central server file list is stale, refreshing...")
+            self.central_files = self.get_central_files()
+            self.last_fetched = time.time()
+            info("Central server file list refreshed.")
+
+        local_files = os.listdir(self.local + path)
+        remote_files = \
+            [f for f in self.central_files.keys() if f not in local_files]
+        return ['.', '..'] + local_files + remote_files
 
     def release(self, path, fh):
+        debug("Releasing file descriptor for %s" % path)
         # file handle is real, use it
         return os.close(fh)
 
-
-class Peer(DatagramProtocol):
-
-    UDP_IP = "239.6.6.6"
-    UDP_PORT = 6666
-    UDP_PORTSend = 6667
-
-    def startProtocol(self):
-        # Set the TTL>1 so multicast will cross router hops:
-        self.transport.setTTL(5)
-        # Join a specific multicast group:
-        self.transport.joinGroup(self.UDP_IP)
-
-    def datagramReceived(self, datagram, address):
-        print "Datagram %s received from %s, %f" % (repr(datagram),
-                                                    repr(address),
-                                                    time.clock())
-        msg = json.loads(datagram)
-        if msg["VERB"] == 'WANT' or msg["VERB"] == 'DOWNLOAD':
-            files = os.listdir(os.getcwd())
-            if msg["FILE"] in files:
-                res = self.compile_message("HAVE", msg["FILE"])
-                self.transport.write(res, (self.UDP_IP, self.UDP_PORTSend))
-
-    def compile_message(self, verb, filename):
-        return json.dumps({"VERB": verb, "FILE": filename})
-
-    def statfs(self, path):
-        return dict(f_bsize=512, f_blocks=4096, f_bavail=2048)
 
 if __name__ == "__main__":
     if len(argv) != 3:
@@ -121,14 +163,6 @@ if __name__ == "__main__":
 
     local = argv[1]
     mountpoint = argv[2]
-
-    peer = Peer()
-
-    # Legacy: respond to UDP multicast protocol
-    # We use listenMultiple=True so that we can run
-    # multiple peers on the same machine
-    reactor.listenMulticast(6666, Peer(),
-                            listenMultiple=True)
 
     # serve files from the local directory over HTTP
     # TODO subclass Site to add count of open connections to be able to
