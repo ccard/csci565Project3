9beba81c42703cb9c92b4e61220ebbb3fd89cf74
Chris Card <ccard@mymail.mines.edu>
2013-11-19 16:37:14 -0700

    it now uses system time diff to clear files when queried for

--- forgetful_cache.py
+++ forgetful_cache.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import datetime
+import time
 import collections
 import threading
 
@@ -9,10 +9,11 @@ class Forgetful_Cache:
 
     """docstring for Forgetful_Cache"""
 
-    def __init__(self, timeout=5):
+    def __init__(self, timeout=10):
         self.lock = threading.Lock()
         self.timeout = timeout
         self.cache = collections.defaultdict()
+        self.timeout_log = {}
 
     def insert(self, filekey, value, peer):
         with self.lock:
@@ -20,21 +21,15 @@ class Forgetful_Cache:
                 if (peer not in self.cache[filekey]['peers'] and
                    value['sha1'] == self.cache[filekey]['sha1']):
                     self.cache[filekey]['peers'].append(peer)
-
-                timer = threading.Timer(
-                    self.timeout, self._purge, [filekey, peer])
-                timer.start()
+                self.timeout_log[filekey][peer] = time.time()
             else:
                 self.cache[filekey] = value
-                timer = threading.Timer(
-                    self.timeout, self._purge, [filekey, peer])
-                timer.start()
+                self.timeout_log[filekey] = {peer: time.time()}
 
     def _purge(self, filekey, peer):
-        with self.lock:
             if filekey not in self.cache:
                 return
-
+            print "removing file:: file: %s, key %s" % (filekey, peer)
             if len(self.cache[filekey]['peers']):
                 if peer in self.cache[filekey]['peers']:
                     self.cache[filekey]['peers'].pop(
@@ -45,6 +40,19 @@ class Forgetful_Cache:
 
     def __getitem__(self, filekey):
         with self.lock:
+            purging = {}
+            for fkey in self.cache:
+                purging[fkey] = []
+                for peer in self.cache[fkey]['peers']:
+                    currtime = time.time()
+                    if ((currtime - self.timeout_log[fkey][peer]) >
+                        self.timeout):
+                        purging[fkey].append(peer)
+
+            for fkey in purging:
+                for peer in purging[fkey]:
+                    self._purge(fkey, peer)
+
             if filekey in self.cache:
                 return self.cache[filekey].copy()
             else:
